cc5c2fce80a4ea5c7503f9c5854c49f645a15e97
Akiva Bamberger <akiva.bamberger@gmail.com>
2018-11-20 10:12:06 -0800

    Adding in a file so we can find templates

--- convert_to_lines.py
+++ convert_to_lines.py
@@ -4,6 +4,7 @@ import sys
 import svg
 import os
 from mkdir_p import mkdir_p
+import json
 
 if len(sys.argv) < 2:
     print 'requires input folder of svg to convert'
@@ -13,7 +14,7 @@ lineDir = 'line_svgs'
 width, height = (128, 128)
 
 
-def convert_svg(inputFilename, outputFilename):
+def convert_svg(inputFilename, outputFilename, dataWriter=None):
     mySvg = svg.Svg(inputFilename)
     f = open(outputFilename, 'w')
     f.write('<svg xmlns="http://www.w3.org/2000/svg" >')
@@ -22,24 +23,32 @@ def convert_svg(inputFilename, outputFilename):
     mySvg.scale(min(width / (bounds[1].x - bounds[0].x), height / (bounds[1].y - bounds[0].y)))
     strokeColor = 'currentcolor'
     fillColor = 'none'
+    f.write('<path d="')
     for path in mySvg.flatten():
         for stroke in path.simplify(100, 1):
-            f.write('<path d="')
-            f.write(''.join(
-                ['M{} {}'.format(stroke[0].x, stroke[0].y), ' '.join(['L{} {}'.format(i.x, i.y) for i in stroke[1:]])]))
-            f.write('" style="stroke: {}; fill: {}"></path>'.format(strokeColor, fillColor))
+            data = ''.join(
+                ['M{:.2f} {:.2f}'.format(stroke[0].x, stroke[0].y), ' '.join(['L{:.2f} {:.2f}'.format(i.x, i.y) for i in stroke[1:]])])
+            f.write(data)
+            if dataWriter:
+                dataWriter.write(data)
+    f.write('" style="stroke: {}; fill: {}"></path>'.format(strokeColor, fillColor))
     f.write('</svg>')
     f.close()
-
+    return data
 
 def convert_dir(oldDir, newDir):
+    dat = open('{}.dat'.format(newDir), 'w')
     mkdir_p(newDir)
     for svgFile in os.listdir(oldDir):
         try:
-            convert_svg(os.path.join(oldDir, svgFile), os.path.join(newDir, svgFile))
+            dat.write('{}: '.format(svgFile))
+            convert_svg(os.path.join(oldDir, svgFile), os.path.join(newDir, svgFile), dataWriter=dat)
+            dat.write('\n')
+
         except Exception:
             print 'Could not convert {}'.format(svgFile)
 
+    dat.close()
 
 if __name__ == '__main__':
     arg = sys.argv[1]
